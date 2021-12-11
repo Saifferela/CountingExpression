2c82f00c91abf95c715d99c563948a006ac63eeb
Saifferela <Saifferela@gmail.com>
2021-12-11 16:32:19 +0200

    Two tests added.

--- src/test/java/CalculationsTest.java
+++ src/test/java/CalculationsTest.java
@@ -1,4 +1,27 @@
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 class CalculationsTest {
-  
+
+    @Test
+    void calculate_ifTheFormulaIsValid_returnNumberTypeDouble() {
+        String formula = "( 10 * 9 ) + ( 3 + (10 / 2))";
+
+        double given = new Calculations().calculate(formula);
+        double expected = 98.0;
+
+        assertThat(given).isEqualTo(expected);
+    }
+
+    @Test
+    void calculate_ifDivisionByZero_returnArithmeticException() {
+        String formula = "( 10 * 9 ) + ( 3 + (10 / 0))";
+
+        ArithmeticException thrown = assertThrows(ArithmeticException.class, () -> new Calculations().calculate(formula));
+
+        assertEquals("/ by zero", thrown.getMessage());
+    }
 }
