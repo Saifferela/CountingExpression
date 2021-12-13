66213bf96f32cebc91730d6e1a9a390b0df3b49a
Saifferela <Saifferela@gmail.com>
2021-12-13 15:10:16 +0200

    Only the condition is left.

--- src/main/java/CountingExpressionApp.java
+++ src/main/java/CountingExpressionApp.java
@@ -2,12 +2,9 @@ public class CountingExpressionApp {
     public static void main(String[] args) {
 
         String formula = "( 10 * 9 ) + ( 3 + ( 10 / 2 ) )";
-        String easyFormula = "10 * 9 / 10 + 5 - 100";
 
         double result = new Calculations().calculate(formula);
-        double easyResult = new Calculations().calculate(easyFormula);
 
-        System.out.println("Expression result: " + result);
-        System.out.println("Expression result: " + easyResult);
+        System.out.println(formula + " = " + result);
     }
 }
