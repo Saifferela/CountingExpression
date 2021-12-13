b3132028bfc74176de0eb34ebb4ef021205ef4b7
Saifferela <Saifferela@gmail.com>
2021-12-13 15:09:37 +0200

    Added tests: * calculate_ifTheFormulaIsValidAndIfTheFormulaContainsMoreThanTwoNestedBrackets * calculate_IfThereIsNoSpace

--- src/test/java/CalculationsTest.java
+++ src/test/java/CalculationsTest.java
@@ -7,8 +7,8 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 class CalculationsTest {
 
     @Test
-    void calculate_ifTheFormulaIsValid_returnNumberTypeDouble() {
-        String formula = "( 10 * 9 ) + ( 3 + (10 / 2))";
+    void calculate_ifTheFormulaIsValid() {
+        String formula = "( 10 * 9 ) + ( 3 + ( 10 / 2 ) )";
 
         double given = new Calculations().calculate(formula);
         double expected = 98.0;
@@ -17,11 +17,30 @@ class CalculationsTest {
     }
 
     @Test
+    void calculate_ifTheFormulaIsValidAndIfTheFormulaContainsMoreThanTwoNestedBrackets() {
+        String formula = "( 10 * 9 ) + ( 3 + ( ( 10 / 2 ) - 3 ) )";
+
+        double given = new Calculations().calculate(formula);
+        double expected = 95.0;
+
+        assertThat(given).isEqualTo(expected);
+    }
+
+    @Test
     void calculate_ifDivisionByZero_returnArithmeticException() {
-        String formula = "( 10 * 9 ) + ( 3 + (10 / 0))";
+        String formula = "( 10 * 9 ) + ( 3 + ( 10 / 0 ) )";
 
         ArithmeticException thrown = assertThrows(ArithmeticException.class, () -> new Calculations().calculate(formula));
 
         assertEquals("/ by zero", thrown.getMessage());
+    }
+
+    @Test
+    void calculate_IfThereIsNoSpace() {
+        String formula = "(10*9)+(3+(10/0))";
+
+        NumberFormatException thrown = assertThrows(NumberFormatException.class, () -> new Calculations().calculate(formula));
+
+        assertEquals("Spaces are not inserted!", thrown.getMessage());
     }
 }
